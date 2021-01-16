24964c403ae66078bc06164909849d4c2dcd2ed4
Victor Kudelin <58315363+K-vic98@users.noreply.github.com>
2021-01-16 20:00:26 +0300

    Made the shooting

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Bullet _bulletTemplate;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_bulletTemplate, _shootPoint);
+        }
+    }
+}
